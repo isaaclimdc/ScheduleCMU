4c61b7328b651135417da55cbaaf889b808e86ac
Naman Bharadwaj <namanbharadwaj@gmail.com>
2012-11-14 16:44:11 -0500

    Steven just playing around with app.js bullshit

--- src/schedulecmu/app.js
+++ src/schedulecmu/app.js
@@ -1,6 +1,6 @@
 var app = require('express').createServer();
 app.get('/', function(req, res) {
-  res.send('Hello from ScheduleCMU!');
+  res.send('<script>alert(\"Hello from ScheduleCMU!\")</script>');
 });
 /* AppFog will pass the listen port as an env var called VCAP_APP_PORT */
 app.listen(process.env.VCAP_APP_PORT || 3000);
