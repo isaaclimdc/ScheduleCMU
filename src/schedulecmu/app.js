220d7d13daec2faf635f17f2dcf997544ce93465
Naman Bharadwaj <nbharadw@andrew.cmu.edu>
2012-11-15 12:19:49 -0500

    Serving UI html by specifically accessing the desktop/ and mobile/ directory in the URL.

--- src/schedulecmu/app.js
+++ src/schedulecmu/app.js
@@ -1,6 +1,7 @@
-var app = require('express').createServer();
-app.get('/', function(req, res) {
-  res.send('<script>alert(\"Hello from ScheduleCMU!\")</script>');
-});
+var express = require('express')
+var app = express();
+
+/* Serve static files from the public directory */
+app.use(express.static('public'));
 /* AppFog will pass the listen port as an env var called VCAP_APP_PORT */
 app.listen(process.env.VCAP_APP_PORT || 3000);
