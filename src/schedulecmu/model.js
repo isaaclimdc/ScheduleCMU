6e330f760454b6dc9c7a1eff8890551e00282d6e
Nivedita Chopra <niveditc@andrew.cmu.edu>
2012-11-24 13:15:06 -0500

    Added a schema for course events within the current model

--- src/schedulecmu/model.js
+++ src/schedulecmu/model.js
@@ -6,6 +6,29 @@ function getCourseModel(Schema, mongoose) {
     Loc: String
   });
 
+  var EventSchema = new Schema({
+    Num: Number,
+    Type: Number, //Since we have a drop down list of 4 types of events                                     
+    Title: String,
+    Loc: String
+    Day: String, //Whether or not we need this depends on the format of recurring events
+    Date: String,
+    Start: String, //TODO? If we use dropdowns, this could be a number                                       
+    End: String, //TODO? If we use dropdowns, this could be number                                      
+    Loc: String,
+
+    /* The state is the sum of reliabilities of people
+     * who have confirmed that the event will take place */
+    State: Number,
+
+    /* The threshold is the minimum value of the state for which we will
+     * consider the event as confirmed and include it in the user's calendar.
+     * This is a field under the event to allow us later on to maybe assign different 
+     * thresholds based on the class strength. For now, we could assign the same number
+     * for all events */
+    Threshold: Number
+  });
+
   var SectionSchema = new Schema();
   SectionSchema.add({
     Num: String,
@@ -22,7 +45,8 @@ function getCourseModel(Schema, mongoose) {
     Description: String,
     URL: String,
     Units: String,
-    Sections: [SectionSchema]
+    Sections: [SectionSchema],
+    CourseEvents: [EventSchema]
   });
 
   return mongoose.model('Course', CourseSchema);
