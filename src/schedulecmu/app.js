c56a766641496510666adacdb281628c79096b25
Naman Bharadwaj <nbharadw@andrew.cmu.edu>
2012-11-29 10:42:10 -0500

    Renaming and restructuring of app.js files.

--- src/schedulecmu/app.js
+++ src/schedulecmu/app.js
@@ -6,95 +6,9 @@ var app = express();
 
 
 var db = require('./db.js').dbConnect(mongoose);
-var Course = require('./model.js')(mongoose, db);
+var Course = require('./course_model.js')(mongoose, db);
 
-app.get('/api/courses', function(req, res) {
-  var query = Course.find({});
-  if (req.query.dept) {
-      query = query.$where('this.num.match(/^' + req.query.dept + '/)'); 
-  }
-  if (req.query.name) {
-
-  }
-  if (req.query.number) {
-    query = query.where('num', req.query.number);  
-  }
-  if (req.query.semester) {
-    query = query.where('semester', parseInt(req.query.semester));
-  }
-  // TODO figure out how to check for minis (best way)
-  // Optional::
-
-  if (req.query.instructor) {
-
-  }
-
-  if (req.query.building) {
-
-  }
-
-  query = query.select("-sections -__v -course_events");
-  query.exec(function(err, courses) {
-    if (err) {
-      console.log(err);
-    }
-    for (var i = 0; i < courses.length; i++) {
-      courses[i].CourseURL = "/api/courses/"
-                           + courses[i]["Semester"]
-                           + courses[i]["Num"];
-    }
-    res.send(JSON.stringify(courses));
-  });
-});
-
-app.get('/api/courses/:course/sections/:section/subsections/:subsec',
-        function(req, res) {
-  //TODO what if courses isn't 10 characters?
-  var course = req.params.course
-  var section = req.params.section.replace("-"," ");
-  var subsection = req.params.subsec;
-  console.log(subsection);
-
-  Course.findById(course, function(err, course) {
-    if (err) {
-      console.log(err);
-      res.send(404);
-    } else {
-      res.send(course.sections.id(section).subsections.id(subsection));
-    }
-  });
-});
-
-app.get('/api/courses/:course/sections/:section', function(req, res) {
-  //TODO what if courses isn't 10 characters?
-  var course = req.params.course;
-  var section = req.params.section.replace("-"," ");
-  console.log(section);
-
-  Course.findById(course, function(err, course) {
-    if (err) {
-      console.log(err);
-      res.send(404);
-    } else {
-      res.send(course.sections.id(section));
-    }
-  });
-});
-
-//For the form /api/courses/131-15-122
-app.get('/api/courses/:course', function(req, res) {
-  var course = req.params.course;
-  console.log(course);
-  Course.findById(course, function(err, course) {
-    if (err) {
-      console.log(err);
-      res.send(404);
-    } else {
-	    res.send(course);
-    }
-  });
-});
-
+require('./query.js')(app, Course);
 
 /* Serve static files from the public directory */
 app.use(express.static('public'));
---
+++ src/schedulecmu/query.js
@@ -0,0 +1,88 @@
+module.exports = function (app, Course) {
+  app.get('/api/courses', function(req, res) {
+    var query = Course.find({});
+    if (req.query.dept) {
+        query = query.$where('this.num.match(/^' + req.query.dept + '/)'); 
+    }
+    if (req.query.name) {
+
+    }
+    if (req.query.number) {
+      query = query.where('num', req.query.number);  
+    }
+    if (req.query.semester) {
+      query = query.where('semester', parseInt(req.query.semester));
+    }
+    // TODO figure out how to check for minis (best way)
+    // Optional::
+
+    if (req.query.instructor) {
+
+    }
+
+    if (req.query.building) {
+
+    }
+
+    query = query.select("-sections -__v -course_events");
+    query.exec(function(err, courses) {
+      if (err) {
+        console.log(err);
+      }
+      for (var i = 0; i < courses.length; i++) {
+        courses[i].CourseURL = "/api/courses/"
+                             + courses[i]["Semester"]
+                             + courses[i]["Num"];
+      }
+      res.send(JSON.stringify(courses));
+    });
+  });
+
+  app.get('/api/courses/:course/sections/:section/subsections/:subsec',
+          function(req, res) {
+    //TODO what if courses isn't 10 characters?
+    var course = req.params.course
+    var section = req.params.section.replace("-"," ");
+    var subsection = req.params.subsec;
+    console.log(subsection);
+
+    Course.findById(course, function(err, course) {
+      if (err) {
+        console.log(err);
+        res.send(404);
+      } else {
+        res.send(course.sections.id(section).subsections.id(subsection));
+      }
+    });
+  });
+
+  app.get('/api/courses/:course/sections/:section', function(req, res) {
+    //TODO what if courses isn't 10 characters?
+    var course = req.params.course;
+    var section = req.params.section.replace("-"," ");
+    console.log(section);
+
+    Course.findById(course, function(err, course) {
+      if (err) {
+        console.log(err);
+        res.send(404);
+      } else {
+        res.send(course.sections.id(section));
+      }
+    });
+  });
+
+  //For the form /api/courses/131-15-122
+  app.get('/api/courses/:course', function(req, res) {
+    var course = req.params.course;
+    console.log(course);
+    Course.findById(course, function(err, course) {
+      if (err) {
+        console.log(err);
+        res.send(404);
+      } else {
+        res.send(course);
+      }
+    });
+  });
+}
