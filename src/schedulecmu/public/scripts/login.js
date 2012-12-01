98adaf41739748b17310b84ceb6ecdd91884d8eb
Isaac Lim <isaaclimdc@gmail.com>
2012-12-01 16:00:26 -0500

    Editing new user

--- src/schedulecmu/public/scripts/login.js
+++ src/schedulecmu/public/scripts/login.js
@@ -89,33 +89,35 @@ $(document).ready(function() {
 function createNewUser() {
     /* Extract FB ID from the url hash */
     var fbID = window.location.hash.substring(1);
-    console.log(fbID);
 
     FB.getLoginStatus(function(response) {
         var andrewID = $("#andrewBox").val();
-        var accessToken;
 
         if (response.status === 'connected') {
-            accessToken = response.authResponse.accessToken;
-        }
+            var accessToken = response.authResponse.accessToken;
 
-        $.ajax({
-            type : "POST",
-            url : "http://schedulecmu.aws.af.cm/api/users/" + fbID,
-            data : {
-                "andrew" : andrewID,
-                "auth_token" : accessToken
-            },
-            success : function(result, status) {
-                console.log(result);
-            },
-            error : function(xhr, status, error) {
-                console.log(error);
-            },
-            statusCode: {
-                200: function() {  },
-                404: function() { console.log("Page not found"); }
-            }
-        });
+            $.ajax({
+                type : "POST",
+                url : "http://schedulecmu.aws.af.cm/api/users/" + fbID,
+                data : {
+                    "andrew" : andrewID,
+                    "auth_token" : accessToken
+                },
+                success : function(result, status) {
+                    console.log(result);
+                    
+                },
+                error : function(xhr, status, error) {
+                    console.log(error);
+                },
+                statusCode: {
+                    200: function() {  },
+                    404: function() { console.log("Page not found"); }
+                }
+            });
+        }
+        else {
+            console.log("You are not logged in to Facebook");
+        }
     });
 }
