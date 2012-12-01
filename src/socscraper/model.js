be28452ba0d5dc60f41bcafe3635655a545bd438
Naman Bharadwaj <nbharadw@andrew.cmu.edu>
2012-11-30 23:58:09 -0500

    turned model in socscraper into a symlink.

--- src/socscraper/model.js
+++ src/socscraper/model.js
@@ -1,42 +1,33 @@
 function getCourseModel(mongoose, db) {
   var Schema = mongoose.Schema;
 
-
-  /* For the recurring schema, I tried to model data accepted by an interface like Google Calendar's
-   * It would be great if we could build a similar interface */
-  var RecurSchema = new Schema({
-    StartDate: {type: Date, default: null},
-    EndDate: {type: Date, default: null},
-    Days: [{type: String, match: /^[MTWRFSU]$/}], //Optional
-    Occurence: {type: Number, default: null}, //Various dropdown options - daily, weekly, monthly, etc
-    Frequency: {type: Number, default: null} //Repeats every n days / weeks / months - depending on the ocurence field
-  });
     
   var EventSchema = new Schema({
     //Num: Number - This should be handled client side, right?
-    Type: {type: Number}, //Since we have a drop down list of 4 types of events       
-    Title: {type: String},
-    Loc: {type: String, default: null} ,
-    DateTime: {type: Date},
+    event_type: {type: Number}, //Since we have a drop down list of 4 types of events       
+    title: {type: String},
+    loc: {type: String, default: null} ,
+    date_time: {type: Date},
 
     /* The state is the sum of reliabilities of people
      * who have confirmed that the event will take place */
-    State: {type: Number, default: 0, min: 0},
+    state: {type: Number, default: 0, min: 0},
 
     /* The threshold is the minimum value of the state for which we will
      * consider the event as confirmed and include it in the user's calendar.
      * This is a field under the event to allow us later on to maybe assign different 
      * thresholds based on the class strength. For now, we could assign the same number
      * for all events */
-    Threshold: {type: Number, min: 0, default: 20},
+    threshold: {type: Number, min: 0, default: 20},
     
-    Recurring: {type: Boolean, default: false},
-    Recur_StartDate: {type: Date, default: null},
-    Recur_EndDate: {type: Date, default: null},
-    Recur_Days: [{type: String, match: /^[MTWRFSU]$/}], //Optional
-    Recur_Occurence: {type: Number, default: null}, //Various dropdown options - daily, weekly, monthly, etc
-    Recur_Frequency: {type: Number, default: null} //Repeats every n days / weeks / months - depending on the ocurence field
-  });
+    recur: {
+      recurring: {type: Boolean, default: false},
+      startDate: {type: Date, default: null},
+      endDate: {type: Date, default: null},
+      days: [{type: String, match: /^[MTWRFSU]$/}], //Optional
+      occurence: {type: Number, default: null}, //Various dropdown options - daily, weekly, monthly, etc
+      frequency: {type: Number, default: null} //Repeats every n days / weeks / months - depending on the ocurence field
+  }});
 
 
 
