df26e31dda75177fe1cd9ec3f8eba73c27598bea
Madeline Rae Horowitz <raemadeline@gmail.com>
2012-12-05 16:33:35 -0500

    Into Resizing in Mobile

--- src/schedulecmu/public/scripts/mobile.js
+++ src/schedulecmu/public/scripts/mobile.js
@@ -1,11 +1,4 @@
 $(document).ready(function() {
-	var docW = $(document).width();
-	if(docW <= 750) {
-		$('#loginGrid').children('.ui-block-a').css('display','none');
-		$('#loginGrid').children('.ui-block-b').removeClass('ui-block-b');
-		$('#loginGrid').removeClass('ui-grid-b');
-	}
-
 	// $('#listview').bind('pageshow', function() {
  //        console.log('List View');
  //        $.mobile.changePage('#listview', {
