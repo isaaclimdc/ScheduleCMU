d5c8b980d59b3209e80f041a0e3d8ea9886e5bb8
Nivedita Chopra <niveditc@andrew.cmu.edu>
2012-12-07 14:54:10 -0500

    cleaned up the courses_api.js file

--- src/schedulecmu/courses_api.js
+++ src/schedulecmu/courses_api.js
@@ -1,240 +1,234 @@
 module.exports = function (app, Course, depts) {
-  app.get('/api/courses', function(req, res) {
-    var query = Course.find({});
-    if (req.query.dept) {
-        var dept = req.query.dept;
-        if(!dept.match(/^[0-9]{2}$/))
-            dept = depts[dept.replace('_', ' ').replace('*', '&').replace('@',':')];
-        query = query.$where('this.num.match(/^' + dept + '/)').sort('num');
-    }
-    if (req.query.name) {
-
-    }
-    if (req.query.number) {
-        var num = req.query.number;
-        	if(num.length == 5)
-	           num = num.substring(0,2) + '-' + num.substring(2);
-	        query = query.where('num', num);
-    }
-    if (req.query.semester) {
-        query = query.where('semester', Number(req.query.semester));
-    }
-    // TODO figure out how to check for minis (best way)
-    // Optional::
-
-    if (req.query.instructor) {
-
-    }
-
-    if (req.query.building) {
-
-    }
-
-    query = query.select("-sections -__v -course_events");
-    query.exec(function(err, courses) {
-      if (err) {
-        console.log(err);
-      }
-      for (var i = 0; i < courses.length; i++) {
-        courses[i].CourseURL = "/api/courses/"
-                             + courses[i]["Semester"]
-                             + courses[i]["Num"];
-      }
-      res.send(JSON.stringify(courses));
-    });
-  });
-
-  app.get('/api/courses/:course/sections/:section/subsections/:subsec',
-          function(req, res) {
-    //TODO what if courses isn't 10 characters?
-    var course = req.params.course
-    var section = req.params.section.replace("-"," ");
-    var subsection = req.params.subsec;
-
-    Course.findById(course, function(err, course) {
-      if (err) {
-        console.log(err);
-        res.send(404);
-      } else {
-        res.send(course.sections.id(section).subsections.id(subsection));
-      }
-    });
-  });
-
-  app.get('/api/courses/:course/sections/:section', function(req, res) {
-    var course = req.params.course;
-    var section = req.params.section.replace("-"," ");
-
-    Course.findById(course, function(err, course) {
-      if (err) {
-        console.log(err);
-        res.send(404);
-      } else {
-        res.send(course.sections.id(section));
-      }
-    });
-  });
-
-  //For the form /api/courses/131-15-122
-  app.get('/api/courses/:course', function(req, res) {
-    var course = req.params.course;
-
-    Course.findById(course, function(err, course) {
-      if (err) {
-        console.log(err);
-        res.send(404);
-      } else {
-        res.send(course);
-      }
-    });
-  });
+    app.get('/api/courses', function(req, res) {
+        var query = Course.find({});
+        if (req.query.dept) {
+            var dept = req.query.dept;
+            if(!dept.match(/^[0-9]{2}$/))
+                dept = depts[dept.replace('_', ' ').
+                             replace('*', '&').replace('@',':')];
+            query = query.$where('this.num.match(/^' + dept + '/)')
+                    .sort('num');
+        }
 
 
-  //returns the entire events array associated with an event
-  app.get('/api/courses/:course/events', function(req, res) {
-	  var course = req.params.course;
+        if (req.query.number) {
+            var num = req.query.number;
+        	  if(num.length == 5)
+	              num = num.substring(0,2) + '-' + num.substring(2);
+	          query = query.where('num', num);
+        }
 
-	  Course.findById(course, function(err, course) {
-		  if (err) {
-		      console.log(err);
-		      res.send(404);
-		  } else {
-		      res.send(course.course_events);
-		  }
-	  });
-  });
+        if (req.query.semester) {
+            query = query.where('semester', Number(req.query.semester));
+        }
 
-
-  app.post('/api/courses/:course/events', function(req, res) {
-    if (req.body.data == undefined) {
-      //400
-      res.send("um no.");
-    } else {
-      // Values for validity of the event
-      req.body.data.threshold = 0;
-      req.body.data.state = 20; //No crowdsourcing for now!
-
-      req.body.data.recur = null; //For now
-    }
-
-	  Course.findById(req.params.course, function(err, course) {
-      if (err || course == undefined) {
-        console.log(err);
-        res.send(404);
-      } else {
-        course.course_events.push(req.body.data);
-        course.save(function (err) {
-          if (err) {
-            console.log(err);
-            res.send(404); //Change to something appropriate
-          } else {
-            res.send(course);
-          }
+        query = query.select("-sections -__v -course_events");
+        query.exec(function(err, courses) {
+            if (err) {
+                console.log(err);
+            }
+            for (var i = 0; i < courses.length; i++) {
+                courses[i].CourseURL = "/api/courses/"
+                    + courses[i]["Semester"]
+                    + courses[i]["Num"];
+            }
+            res.send(JSON.stringify(courses));
         });
-      }
     });
-  });
 
-  app.del('/api/courses/:course/events', function(req, res) {
-      Course.findById(req.params.course, function(err, course){
-          if(err || (course == undefined)){
-              res.send(404, {error: "Course is not defined"});                                                                                                       }
-          course.course_events.remove(function(err){
-              if(err){
-                  res.send(404, {error: "We messed up somewhere"});
-              } else{
-                  course.course_events = [];
-                  course.save(function(err){
-                      if(err){
-                          res.send(404, {error: "We messed up somewhere"});
-                      }
-                      else{
-                          res.send(course);
-                      }
-                  });
-              }
-          });
-      });
-  });
+    app.get('/api/courses/:course/sections/:section/subsections/:subsec',
+            function(req, res) {
+                var course = req.params.course;
+                var section = req.params.section.replace("-"," ");
+                var subsection = req.params.subsec;
 
+                Course.findById(course, function(err, course) {
+                    if (err) {
+                        console.log(err);
+                        res.send(404, {error: "Course not found"});
+                    } else {
+                        res.send(course.sections.id(section).
+                                 subsections.id(subsection));
+                    }
+                });
+    });
 
-  app.del('/api/courses/:course/events/:event', function(req, res) {
-      Course.findById(req.params.course, function(err, course){
-          if(err || (course == undefined)){
-              res.send(404, {error: "Course is not defined"});
-          }
-          var event = course.course_events.id(req.params.event);
-          if(event == undefined){
-              res.send(404, {error: "Schedule not found"});
-          }
+    app.get('/api/courses/:course/sections/:section', function(req, res) {
+            var course = req.params.course;
+            var section = req.params.section.replace("-"," ");
 
-          event.remove(function(err){
-              if(err){
-                  res.send(404, {error: "We messed up somewhere"});
-              }
-              else{
-                  course.save(function(err){
-                      if(err){
-                          res.send(404, {error: "We messed up somewhere"});
-                      }
-                      else{
-                          res.send(course);
-                      }
-                  });
-              }
+            Course.findById(course, function(err, course) {
+                if (err) {
+                    console.log(err);
+                    res.send(404, {error: "Course not found"});
+                } else {
+                    res.send(course.sections.id(section));
+                }
+            });
+    });
 
-         });
-     });
-  });
+    //For the form /api/courses/131-15-122
+    app.get('/api/courses/:course', function(req, res) {
+            var course = req.params.course;
+
+            Course.findById(course, function(err, course) {
+                if (err) {
+                    console.log(err);
+                    res.send(404, {error: "Course not found"});
+                } else {
+                    res.send(course);
+                }
+           });
+    });
 
 
-  app.put('/api/courses/:course/events/:event',
-          function (req, res) {
-              if (req.body.data == undefined) {
-                  res.send(400, {error: "No event received."});
-                  return;
-              }
-              req.body.data._id = req.params.event;
+    /*returns the entire events array associated with an event */
+    app.get('/api/courses/:course/events', function(req, res) {
+            var course = req.params.course;
 
-              Course.findById(req.params.course, function(err, course){
-                  if(err) {
-                      console.log(err);
-                      res.send(500, {error: "No database connection."});
-                      return;
-                  }
-                  if (course == undefined) {
-                      res.send(404, {error: "Course not found."});
-                      return;
-                  }
+            Course.findById(course, function(err, course) {
+                if (err) {
+                    console.log(err);
+                    res.send(404, {error: "Course not found"});
+                } else {
+                    res.send(course.course_events);
+                }
+            });
+    });
 
-                  var event = course.course_events.id(req.params.event);
-                  if (event == undefined) {
-                      course.course_events.push(req.body.data);
-                  } else {
-                      if(req.body.data.event_type)
-                          event.event_type = Number(req.body.data.event_type);
-                      if(req.body.data.title)
-                          event.title = req.body.data.title;
-                      if(req.body.data.loc)
-                          event.loc = req.body.data.loc;
-                      if(req.body.data.state)
-                          event.state = req.body.data.state;
-                      if(req.body.data.threshold)
-                          event.threshold = req.body.data.threshold;
-                      if(req.body.data.recur)
-                          event.recur = req.body.data.recur;
-                  }
 
-                  course.save(function(err){
-                      if(err){
-                          res.send(400, {error: "Invalid event syntax."});
-                          console.log(err);
-                      } else {
-                          res.send(course.course_events);
-                  }
-             });
+    app.post('/api/courses/:course/events', function(req, res) {
+        if (req.body.data == undefined) {
+            console.log(err);
+            res.send(400, {error: "No event received."});
+        } else {
+            req.body.data.recur = null;
+        }
+
+        Course.findById(req.params.course, function(err, course) {
+            if (err || course == undefined) {
+                console.log(err);
+                res.send(404, {error: "Course is not defined"});
+            } else {
+                course.course_events.push(req.body.data);
+                course.save(function (err) {
+                    if (err) {
+                        console.log(err);
+                        res.send(404, {error: "We messed up somewhere"});
+                    } else {
+                        res.send(course);
+                    }
+                });
+            }
        });
-   });
+    });
+
+    app.del('/api/courses/:course/events', function(req, res) {
+        Course.findById(req.params.course, function(err, course){
+            if(err || (course == undefined)){
+                console.log(err);
+                res.send(404, {error: "Course is not defined"});                                                                                                       }
+            course.course_events.remove(function(err){
+                if(err){
+                    console.log(err);
+                    res.send(404, {error: "We messed up somewhere"});
+                } else{
+                    course.course_events = [];
+                    course.save(function(err){
+                        if(err){
+                            console.log(err);
+                            res.send(404, {error: "We messed up somewhere"});
+                        }
+                        else{
+                            res.send(course);
+                        }
+                    });
+                }
+            });
+        });
+    });
 
 
-      }
+    app.del('/api/courses/:course/events/:event', function(req, res) {
+        Course.findById(req.params.course, function(err, course){
+            if(err || (course == undefined)){
+                console.log(err);
+                res.send(404, {error: "Course is not defined"});
+            }
+            var event = course.course_events.id(req.params.event);
+            if(event == undefined){
+                console.log(err);
+                res.send(404, {error: "Schedule not found"});
+            }
+
+            event.remove(function(err){
+                if(err){
+                    console.log(err);
+                    res.send(404, {error: "We messed up somewhere"});
+                }
+                else{
+                    course.save(function(err){
+                        if(err){
+                            console.log(err);
+                            res.send(404, {error: "We messed up somewhere"});
+                        }
+                        else{
+                            res.send(course);
+                        }
+                    });
+                }
+           });
+       });
+    });
+
+
+    app.put('/api/courses/:course/events/:event',
+            function (req, res) {
+                if (req.body.data == undefined) {
+                    console.log(err);
+                    res.send(400, {error: "No event received."});
+                    return;
+                }
+                req.body.data._id = req.params.event;
+
+                Course.findById(req.params.course, function(err, course){
+                    if(err) {
+                        console.log(err);
+                        res.send(500, {error: "No database connection."});
+                        return;
+                    }
+                    if (course == undefined) {
+                        console.log(err);
+                        res.send(404, {error: "Course not found."});
+                        return;
+                    }
+
+                    var event = course.course_events.id(req.params.event);
+                    if (event == undefined) {
+                        course.course_events.push(req.body.data);
+                    } else {
+                        if(req.body.data.event_type)
+                            event.event_type = Number(req.body.data.event_type);
+                        if(req.body.data.title)
+                            event.title = req.body.data.title;
+                        if(req.body.data.loc)
+                            event.loc = req.body.data.loc;
+                        if(req.body.data.state)
+                            event.state = req.body.data.state;
+                        if(req.body.data.threshold)
+                            event.threshold = req.body.data.threshold;
+                        if(req.body.data.recur)
+                            event.recur = req.body.data.recur;
+                    }
+
+                    course.save(function(err){
+                        if(err){
+                            console.log(err);
+                            res.send(400, {error: "Invalid event syntax."});
+                        } else {
+                            res.send(course.course_events);
+                        }
+                    });
+             });
+     });
+}
