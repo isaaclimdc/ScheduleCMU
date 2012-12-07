94858af4902b92619aa988a1b92578efe83c9ec1
Nivedita Chopra <niveditc@andrew.cmu.edu>
2012-12-07 14:05:02 -0500

    cleaned up the user_model.js file

--- src/schedulecmu/user_model.js
+++ src/schedulecmu/user_model.js
@@ -1,115 +1,131 @@
 request = require("request");
 
-// SHH IT'S A SECRET
+/* SHH IT'S A SECRET */
 var app_access = "102585986572914|a_j0-_3SoiGwgLvcemx0gYE3jQo"
 
 module.exports = function (mongoose, db) {
-  var Schema = mongoose.Schema;
+    var Schema = mongoose.Schema;
 
-  var BlockSchema = new Schema({
-    _id: {type: String},
-    section: {type: Number},
-    subsection: {type: Number, default: null}
-  });
+    var BlockSchema = new Schema({
+        _id: {type: String},
+        section: {type: Number},
+        subsection: {type: Number, default: null}
+    });
 
-  var ScheduleSchema = new Schema({
-    semester: {type: Number, min: 130, max: 999},
-    name : {type: String},
-    course_blocks: [BlockSchema]
-  });
+    var ScheduleSchema = new Schema({
+        semester: {type: Number, min: 130, max: 999},
+        name : {type: String},
+        course_blocks: [BlockSchema]
+    });
 
-  var UserSchema = new Schema({
-    // the _id field should store the fbid of the user
-    _id: {type: String},
-    andrew: {type: String, match: /^[A-Za-z0-9]{3,8}$/},
-    auth_token: {type: String, default: null},
-    auth_expiry: {type: Number, default: null},
-    verify_code: {type: String, default: null},
-    schedules: [ScheduleSchema]
-  });
+    var UserSchema = new Schema({
 
-  UserSchema.methods.authenticate = function (auth_token, cb) {
-    // Makes the user accessible in future callbacks
-    var curUser = this;
+        /* the _id field should store the Facebook id of the user */
+        _id: {type: String},
 
-    var currentTime = new Date().getTime() / 1000;
-    if (this.auth_token == auth_token && currentTime < this.auth_expiry) {
-      if (this.verify_code == null)
-        cb("valid");
-      else
-        cb("verify");
-      return;
+        /* this stores the andrew id of the user */
+        andrew: {type: String, match: /^[A-Za-z0-9]{3,8}$/},
+
+        /* these are obtained from Facebook */
+        auth_token: {type: String, default: null},
+        auth_expiry: {type: Number, default: null},
+        verify_code: {type: String, default: null},
+
+        schedules: [ScheduleSchema]
+    });
+
+
+    UserSchema.methods.authenticate = function (auth_token, cb) {
+
+        /* makes the user accessible in future callbacks */
+        var curUser = this;
+
+        var currentTime = new Date().getTime() / 1000;
+        if (this.auth_token == auth_token && currentTime < this.auth_expiry) {
+            if (this.verify_code == null)
+                cb("valid");
+            else
+                cb("verify");
+            return;
+        }
+
+        /* if not a match, we need to verify with Facebook... */
+        var debug_url = "https://graph.facebook.com/debug_token?input_token=" +
+                         auth_token + "&access_token=" + app_access;
+        request.get({url:debug_url, json:true}, function (err, res, body) {
+            if (err) {
+                console.log (err);
+                /* this is really bad - we don't know if the user is valid */
+                cb("invalid");
+                return;
+            }
+
+            if (body.data == undefined) {
+                cb("invalid");
+                return;
+            }
+
+            if (curUser._id == body.data.user_id && body.data.is_valid) {
+                curUser.auth_token = auth_token;
+                curUser.auth_expiry = body.data.expires_at;
+                curUser.save(function (err) {
+                    console.log(err);
+                });
+                if (curUser.verify_code == null)
+                    cb("valid");
+                else
+                    cb("verify");
+            } else {
+                console.log("Invalid");
+                cb("invalid");
+            }
+      });
     }
 
-    // If not a match, we need to verify with facebook...
-    var debug_url = "https://graph.facebook.com/debug_token?input_token=" +
-                    auth_token + "&access_token=" + app_access;
-    request.get({url:debug_url, json:true}, function (err, res, body) {
-      if (err) {
-        console.log (err);
-        // This is really bad - we don't know if the user is valid
-        cb("invalid");
-        return;
-      }
+    UserSchema.methods.sendVerifyEmail = function(){
+        var nodemailer = require("nodemailer");
 
-      if (body.data == undefined) {
-        cb("invalid");
-        return;
-      }
-
-      console.log(body);
-      console.log(this);
-
-      if (curUser._id == body.data.user_id && body.data.is_valid) {
-        curUser.auth_token = auth_token;
-        curUser.auth_expiry = body.data.expires_at;
-        curUser.save(function (err) {
-          console.log(err);
-          // Not the end of the world
+        var smtpTransport = nodemailer.createTransport("SMTP",{
+            service: "Gmail",
+            auth: {
+                user: "schedulecmu@gmail.com",
+                pass: "$chedule"
+            }
         });
-        if (curUser.verify_code == null)
-          cb("valid");
-        else
-          cb("verify");
-      } else {
-        console.log("Invalid");
-        cb("invalid");
-      }
-    });
-  }
 
-  UserSchema.methods.sendVerifyEmail = function(){
-    var nodemailer = require("nodemailer");
+        var mailOptions = {
+            /* sender address */
+            from: "ScheduleCMU.org <admin@schedulecmu.org>",
 
-    // create reusable transport method (opens pool of SMTP connections)
-    var smtpTransport = nodemailer.createTransport("SMTP",{
-      service: "Gmail",
-      auth: {
-        user: "schedulecmu@gmail.com",
-        pass: "$chedule"
-      }
-    });
+            /* list of receivers */
+            to: this.andrew + "@andrew.cmu.edu",
 
-    // setup e-mail data with unicode symbols
-    var mailOptions = {
-      from: "ScheduleCMU.org <admin@schedulecmu.org>", // sender address
-      to: this.andrew + "@andrew.cmu.edu", // list of receivers
-      subject: "Verify your account at ScheduleCMU", // Subject line
-      text: "Hi " + this.andrew + ",\nPlease go to the following url to verify your email address:\nhttp://schedulecmu.aws.af.cm/verify.html#" + this.verify_code  + "\nThis is an auto-generated email. Please do not reply to this mail.",
-      html: "Hi " + this.andrew + ",<p>Please click <span><a href='http://schedulecmu.aws.af.cm/verify.html#" + this.verify_code + "'>here</a></span> to verify your account.</p><p>-- ScheduleCMU Admin</p><p style='font-size: 10px;'>This is an auto-generated email. Please do not reply to this mail.</p>"
+            /* subject line */
+            subject: "Verify your account at ScheduleCMU",
+
+            /* plaintext version of the message body */
+            text: "Hi " + this.andrew + ",\nPlease go to the following url to verify" +
+            "your email address:\nhttp://schedulecmu.aws.af.cm/verify.html#" + this.verify_code +
+            "\nThis is an auto-generated email. Please do not reply to this mail.",
+
+            /* html version of the message body */
+            html: "Hi " + this.andrew + ",<p>Please click <span>" +
+            "<a href='http://schedulecmu.aws.af.cm/verify.html#" + this.verify_code +
+            "'>here</a></span> to verify your account.</p><p>-- ScheduleCMU Admin</p>" +
+            "<p style='font-size: 10px;'>This is an auto-generated email." +
+            " Please do not reply to this mail.</p>"
+        }
+
+        /* send mail with defined transport object */
+        smtpTransport.sendMail(mailOptions, function(error, response){
+            if (error) {
+                console.log(error);
+            }
+            else {
+                console.log("Message sent: " + response.message);
+            }
+        });
     }
 
-    // send mail with defined transport object
-    smtpTransport.sendMail(mailOptions, function(error, response){
-      if (error) {
-        console.log(error);
-      }
-      else {
-        console.log("Message sent: " + response.message);
-      }
-	  });
-  }
-
-  return db.model('User', UserSchema);
-
+    return db.model('User', UserSchema);
 }
